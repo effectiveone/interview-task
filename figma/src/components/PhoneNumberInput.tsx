164804bcea251a78a1d8d4b089ab202fa5a01c62
Konrad <konrad.gruca90@gmail.com>
2023-07-11 09:59:10 +0200

    added logic to select country

--- figma/src/components/PhoneNumberInput.tsx
+++ figma/src/components/PhoneNumberInput.tsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./PhoneNumberInput.scss";
 import Countries from "../service/utils/countries.json";
+import { UpArrowIcon, DownArrowIcon } from "../service/utils/icons";
 
 interface Country {
   name: string;
-  alpha2Code: string;
-  dialCode: string;
+  code: string;
+  dial_code: string;
   flag: string;
 }
 
 const PhoneNumberInput: React.FC = () => {
   const [countryCode, setCountryCode] = useState("");
   const [phone, setPhone] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState<
+    Country | null | undefined
+  >(Countries[0]);
+  const [prefix, setPrefix] = useState("");
+  const [openDropdown, setOpenDropdown] = useState(false);
 
   const fetchCountryCode = (countryName: string) => {
-    const country: any | undefined = Countries.find((item: any) =>
+    const country = Countries.find((item: Country) =>
       item.name.toLowerCase().includes(countryName.toLowerCase())
     );
     if (country) {
@@ -35,12 +41,99 @@ const PhoneNumberInput: React.FC = () => {
     }
   };
 
+  const selectCountry = (selectedPropCountry: Country) => {
+    setSelectedCountry(selectedPropCountry);
+    setOpenDropdown(false);
+    setPrefix(selectedPropCountry.dial_code);
+  };
+
+  useEffect(() => {
+    console.log("selectedCountry changed:", selectedCountry);
+  }, [selectedCountry]);
+
   return (
     <div className="PhoneNumber__Box">
       <h3 className="PhoneNumber__Heading">Change phone number</h3>
-      <input type="text" value={phone} onChange={handleChange} />
-      {countryCode && <div>{countryCode}</div>}
+      {selectedCountry && (
+        <SelectedCountryBox
+          openDropdown={openDropdown}
+          selectedCountry={selectedCountry}
+          open={setOpenDropdown}
+        />
+      )}
+      {openDropdown && (
+        <>
+          <input type="text" onChange={handleChange} placeholder="Search" />
+          {countryCode && <div>{countryCode}</div>}
+          {!countryCode && (
+            <CountriesSelect
+              selectCountry={selectCountry}
+              countries={Countries}
+            />
+          )}
+        </>
+      )}
     </div>
+  );
+};
+
+type CountriesSelectProps = {
+  selectCountry: (country: Country) => void;
+  countries: Country[] | null | undefined;
+};
+
+const CountriesSelect: React.FC<CountriesSelectProps> = ({
+  selectCountry,
+  countries,
+}) => (
+  <>
+    <div className="PhoneNumber__Wrapper">
+      {countries?.map((country: Country, index: number) => (
+        <div
+          className="PhoneNumber__listItems"
+          key={index}
+          onClick={() => selectCountry(country)}
+        >
+          <div>
+            {country.flag}
+            {country.name}
+          </div>
+          <div className="PhoneNumber__digicode">{country.dial_code}</div>{" "}
+        </div>
+      ))}
+    </div>
+  </>
+);
+
+type SelectedCountryBoxProps = {
+  open: (value: boolean) => void;
+  openDropdown: boolean;
+  selectedCountry: Country | null | undefined;
+};
+
+const SelectedCountryBox: React.FC<SelectedCountryBoxProps> = ({
+  selectedCountry,
+  open,
+  openDropdown,
+}) => {
+  useEffect(() => {
+    console.log("selectedCountry changed:", selectedCountry);
+  }, [selectedCountry]);
+
+  return (
+    <>
+      <div className="PhoneNumber__countrybox">
+        {selectedCountry && (
+          <>
+            {selectedCountry.flag}
+            {selectedCountry.dial_code}
+          </>
+        )}
+        <div className="PhoneNumber__Icon" onClick={() => open(!openDropdown)}>
+          {openDropdown ? <UpArrowIcon /> : <DownArrowIcon />}
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ figma/src/service/utils/icons.js
@@ -0,0 +1,21 @@
+export const UpArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="10"
+    height="10"
+  >
+    <path d="M12 3L1 15h22L12 3z" />
+  </svg>
+);
+
+export const DownArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="10"
+    height="10"
+  >
+    <path d="M12 21L23 9H1l11 12z" />
+  </svg>
+);
